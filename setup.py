e9538f5dd0251f25957655a1905100de16b109a0
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-04 11:43:13 +0200

    Python package.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='granoloader',
+    version='0.1.0',
+    description="Import data to grano using CSV files.",
+    long_description="",
+    classifiers=[
+        ],
+    keywords='data client rest grano sna ddj journalism',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='https://github.com/granoproject/granoloader',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    zip_safe=False,
+    install_requires=[
+        "grano-client>=0.3",
+        "unicodecsv>=0.9.4"
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
