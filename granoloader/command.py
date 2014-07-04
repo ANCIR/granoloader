7e8c602228ebc75ea6da92ecee35b9864eeb8568
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-04 15:57:56 +0200

    First working implementation of the CSV loader

--- granoloader/command.py
+++ granoloader/command.py
@@ -1,18 +1,23 @@
 import click
+import logging
 import yaml
 from unicodecsv import DictReader
-from granoclient import Grano
+from granoclient import Grano, GranoException
 
-from granoloader.mapping import MappingLoader
+from granoloader.mapping import MappingLoader, RowException
+
+logging.basicConfig(level=logging.WARN)
+log = logging.getLogger()
 
 
 def make_client(host, project_name, api_key):
-    """ Instantiate the grano client based on environment variables or 
+    """ Instantiate the grano client based on environment variables or
     command line settings. """
     if host is None:
         raise click.BadParameter('No grano server host is set', param=host)
     if project_name is None:
-        raise click.BadParameter('No grano project slug is set', param=project_name)
+        raise click.BadParameter('No grano project slug is set',
+                                 param=project_name)
     if api_key is None:
         raise click.BadParameter('No grano API key is set', param=api_key)
 
@@ -28,9 +33,11 @@ def make_client(host, project_name, api_key):
               help='Project slug to be loaded')
 @click.option('--api-key', '-k', envvar='GRANO_APIKEY',
               help='API key with write access to the project')
+@click.option('--force', '-f', default=False, is_flag=True,
+              help='Continue loading upon errors')
 @click.argument('mapping', type=click.File('rb'))
 @click.argument('data', type=click.File('rb'))
-def load(host, project, api_key, mapping, data):
+def load(host, project, api_key, force, mapping, data):
     """ Load CSV data into a grano instance using a mapping specification. """
 
     # Find out how many lines there are (for the progress bar).
@@ -45,9 +52,18 @@ def load(host, project, api_key, mapping, data):
     mapping_loader = MappingLoader(grano, mapping)
 
     with click.progressbar(DictReader(data),
-                           label='Loading',
+                           label=data.name,
                            length=lines) as bar:
-        for row in bar:
-            from time import sleep
-            sleep(1)
-            mapping_loader.load(row)
+        for i, row in enumerate(bar):
+            try:
+                mapping_loader.load(row)
+            except GranoException, ge:
+                msg = '\nServer error: %s' % ge.message
+                click.secho(msg, fg='red', bold=True)
+                if not force:
+                    return -1
+            except RowException, re:
+                msg = '\nRow %s: %s' % (i, re.message)
+                click.secho(msg, fg='red', bold=True)
+                if not force:
+                    return -1
--- granoloader/mapping.py
+++ granoloader/mapping.py
@@ -1,10 +1,109 @@
+from granoclient.loader import Loader
+
+
+def is_empty(value):
+    if value is None or not len(value.strip()):
+        return True
+    return False
+
+
+class RowException(Exception):
+    pass
+
+
+class ObjectMapper(object):
+
+    def __init__(self, name, model, columns):
+        self.name = name
+        self.model = model
+        self.columns = []
+        for column in columns:
+            if column.get('object') == self.name:
+                self.columns.append(column)
+
+    def get_value(self, spec, row):
+        column = spec.get('column')
+        if column is None:
+            return
+        value = row.get(column)
+        if is_empty(value):
+            return None
+        return value
+
+    def get_source(self, spec, row):
+        """ Sources can be specified as plain strings or as a reference to a column. """
+        value = self.get_value({'column': spec.get('source_url_column')}, row)
+        if value is not None:
+            return value
+        return spec.get('source_url')
+
+    def load_properties(self, obj, row):
+        source_url = self.get_source(self.model, row)
+
+        for column in self.columns:
+            col_source_url = self.get_source(column, row)
+            col_source_url = col_source_url or source_url
+
+            value = self.get_value(column, row)
+            if value is None and column.get('required', False):
+                raise RowException('%s is not valid: %s' % (
+                    column.get('column'), row.get(column.get('column'))))
+            if value is None and column.get('skip_empty', True):
+                continue
+            obj.set(column.get('property'), value,
+                    source_url=source_url)
+
+
+class EntityMapper(ObjectMapper):
+
+    def load(self, loader, row):
+        source_url = self.get_source(self.model, row)
+        entity = loader.make_entity(self.model.get('schemata'),
+                                    source_url=source_url)
+        self.load_properties(entity, row)
+        entity.save()
+        return entity
+
+
+class RelationMapper(ObjectMapper):
+
+    def load(self, loader, row, objs):
+        source_url = self.get_source(self.model, row)
+        source = objs.get(self.model.get('source'))
+        target = objs.get(self.model.get('target'))
+        if None in [source, target]:
+            return
+        relation = loader.make_relation(self.model.get('schema'),
+                                        source, target,
+                                        source_url=source_url)
+        self.load_properties(relation, row)
+        relation.save()
+        return relation
 
 
 class MappingLoader(object):
 
-    def __init__(self, grano, mapping):
+    def __init__(self, grano, model):
         self.grano = grano
-        self.data = mapping
+        self.loader = Loader(grano, source_url=model.get('source_url'))
+        self.model = model
+
+    @property
+    def entities(self):
+        for name, model in self.model.get('entities').items():
+            yield EntityMapper(name, model, self.model.get('columns'))
+
+    @property
+    def relations(self):
+        for name, model in self.model.get('relations').items():
+            yield RelationMapper(name, model, self.model.get('columns'))
 
     def load(self, data):
-        pass
+        """ Load a single row of data and convert it into entities and
+        relations. """
+        objs = {}
+        for mapper in self.entities:
+            objs[mapper.name] = mapper.load(self.loader, data)
+
+        for mapper in self.relations:
+            objs[mapper.name] = mapper.load(self.loader, data, objs)
